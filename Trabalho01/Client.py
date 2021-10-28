13b74d1ec12fa3c1028ddf26b723ce26267ec932
Matheus Fonseca <matheus.2016@alunos.utfpr.edu.br>
2021-10-28 16:06:16 -0300

    Client skeleton created

--- Trabalho01/Client.py
+++ Trabalho01/Client.py
@@ -1,66 +1,164 @@
 
-# Client 
+import Pyro4
+import sys
+import time
 
-import secrets
 from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
 
-# https://cryptography.io/en/latest/hazmat/primitives/asymmetric/rsa/
-# https://nitratine.net/blog/post/asymmetric-encryption-and-decryption-in-python/
+# GLOBAL CONTROLLERS
+menuFlag = False
+userLogged = False
+@Pyro4.expose
+@Pyro4.callback
+class Client(object):
+    def __init__(self, name):
+        self.name = name
 
-# RSA Algorithm
+    def notify(self):
+        print("Callback received")
 
-def generateAsymmetricKeys(exponent, keysize):
-    private_key = rsa.generate_private_key(
-    public_exponent=exponent,
-    key_size=keysize,
-    backend=default_backend()
-    )
-    public_key = private_key.public_key()
+    #def loopThread(daemon):
 
-    return public_key, private_key
+    def privateKey(self):
+        self.private_key = rsa.generate_private_key(
+                public_exponent=65537,
+                key_size=1024,
+            )
 
-def signIn(username, password):
-    # TODO: Sign In Method for logging users.
+    def publicKey(self):
+        return self.private_key.public_key()
+    
+    def sign(self):
+        message = b"A message I want to sign"
+        signature = self.private_key.sign(
+            message,
+            padding.PSS(
+                mgf=padding.MGF1(hashes.SHA256()),
+                salt_length=padding.PSS.MAX_LENGTH
+            ),
+            hashes.SHA256()
+        )
+
+def login():
+    print('oooooi, josuke!')
+
+def printMenu(option = 0, username = 'Default'):
+
+    if option == 0:
+        print("------------------------------")
+        print("====== Welcome to Doodle!, please select options below: ======")
+        print(" z - Create Account")
+        print(" x - Login")
+        print(" c - Leave")
+        print("====== Tip: The menu is not case sensitive ======")        
+        print("------------------------------")
+
+    if option == 1:
+        print("------------------------------")
+        print("====== Create Account Menu ======")
+        print("====== Tip: The credentials will be case sensitive ======")
+
+    if option == 2:
+        print("------------------------------")
+        print("====== Login Menu ======")
+        print("====== Tip: The credentials will be case sensitive ======")
+
+    if option == 3:
+        print("------------------------------")
+        print("====== Welcome! " + str(username) + "======")
+
+    if option == 4:
+        print("------------------------------")
+        print("====== Please select options below: ======")
+        print(" z - Create new Survey")
+        print(" x - Vote for open surveys")
+        print(" c - Check for ongoing surveys ")        
+        print(" v - Check for updates")
+        print(" b - Leave | Shut down")
+        print("====== Tip: The menu is not case sensitive ======")   
+
+    
+    if option == 6:
+        print("------------------------------")
+        print("====== Thanks for using our services! ======")
+
+def main():
+
+    # Step 1 - Init client.py and locate server named survey.server
+    ns = Pyro4.locateNS()
+    uri = ns.lookup("survey.server")
+    surveyServer = Pyro4.Proxy(uri)
+
+    # Step 2 - After connected to server, presents the main menu to the client.
+    if not surveyServer:
+        print("Sorry, something went wrong when connecting to the server. Please try again.")
+        sys.exit()
+    else:
+        menuFlag = True
+
+    while(menuFlag):
+        printMenu(0)        
+        choice = input("Input: ")
+
+        # Create Account Menu
+        if(choice == 'z' or choice == 'Z'):
+            printMenu(1)
+            username = input("Write your desired username: ")
+            
+            # TODO: Server method to create account.
+            # If successful, we create client with that username.
+            client = Client(username)
+
+            print(" Account created successfully! ")
+            time.sleep(5)
+
+        # Login Menu
+        elif(choice == 'x' or choice == 'X'):
+            printMenu(2)
+            username = input("Username: ")
+
+            # TODO: Server method to check if credentials are correct.
+            printMenu(3,username)
+            menuFlag = False
+            userLogged = True
+
+        elif(choice == '3'):
+            print("Thank you! Leaving now...")
+            sys.exit()
+        
+    while(userLogged):
+        printMenu(4)
+        choice = input("Input: ")
+
+        # Choice 1 - Create New Survey
+        if (choice == 'z' or choice == 'Z'):
+            # TODO: Menu for necessary information.
+            print(" Welcome to our survey creator! Below, you will find every information necessary to create your survey.")
+            # TODO: Server method to add new survey.
 
 
-    return -1
+        # Choice 2 - Vote for open surveys
+        elif (choice == 'x' or choice == 'X'):
+            # TODO: Server method to vote for existing survey.
+            print(" Below you will find all surveys that required your attention. Please, choose one open survey to start.")
 
-def checkUsername(username):
-    # TODO: Check username
+        # Choice 3 - Check for ongoing surveys
+        elif (choice == 'c' or choice == 'C'):
+            # TODO: Server method to check ongoing status for surveys.
+            print(" Please, inform what survey you want to know more about.")
+            print(" REMINDER! YOU CAN ONLY CHECK SURVEY STATUS FOR SURVEYS YOU ALREADY PARTICIPATED!")
+            # TODO: This message needs to be signed.
+            # serverFunc(client.sign(message))
 
-    return -1
+        # Choice 4 - Check for updates
+        elif (choice == 'v' or choice == 'V'):
+            # TODO: "client...?" method to receive notifications.
+            client.notify()
 
-def signUp(username, password):
-    # TODO: Sign Up Method for registering users.
+        # Choice 5 - Leave the program
+        elif (choice == 'b' or choice == 'B'):
+            print("Thank you! Leaving now...")
+            sys.exit()
 
-    # TODO: Generate exponent e keysize randomly for each user. Use 'secrets' library for that.
-    # userkeys = generateAsymmetricKeys(exponent, keysize)
-
-    # TODO: Verify if current username is already taken.
-    # storeUserCredentials(username, password, userkeys)
-
-    return -1
-
-def storeUserCredentials(username, password, userkeys):
-
-    # Step 1 - Serialize keys created by RSA Algorithm.
-
-    private_key = userkeys[1]
-    public_key = userkeys[0]
-
-    pvt_key = private_key.private_bytes(
-    encoding=serialization.Encoding.PEM,
-    format=serialization.PrivateFormat.PKCS8,
-    encryption_algorithm=serialization.NoEncryption()
-    )
-
-    pb_key = public_key.public_bytes(
-    encoding=serialization.Encoding.PEM,
-    format=serialization.PublicFormat.SubjectPublicKeyInfo
-    )
-
-    # TODO: Create file to store user credentials.
-
-    return -1
